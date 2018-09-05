919edd6aecb81b5af547375c86fb190a70ea5426
Ryoma Ohira <r.j.ohira@gmail.com>
2018-09-05 22:47:39 +1000

    Cosmetic updates

--- chat-app/src/app/app.module.ts
+++ chat-app/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http'; 
 
 @NgModule({
   declarations: [
@@ -13,8 +15,10 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- chat-app/src/app/login/login.component.ts
+++ chat-app/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public username:string;
+  private password:string;
 
-  constructor() { }
+  constructor(private router:Router, private form:FormsModule, private _userService:UserService) {}
 
   ngOnInit() {
+  }
+
+  loginUser(event){
+    event.preventDefault();
+    console.log(this.username);
+    console.log(this.password);
+    let user = {
+      'username': this.username,
+      'password': this.password
+    }
+
+    
+    this._userService.login(user).subscribe(
+      data => { 
+        console.log(data);
+        if(data != false){
+          let temp = JSON.stringify(data);
+          sessionStorage.setItem('user', temp);
+          sessionStorage.setItem('username', this.username);
+          //return true;
+          
+         this.router.navigate(['/home']); 
+        }
+      },
+      error => {
+        console.error(error);
+      }
+    )
   }
 
 }
