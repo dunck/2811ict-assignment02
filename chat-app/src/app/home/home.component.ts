b4cac12c95cbc66715d437c9b627809ae642140e
Ryoma Ohira <r.j.ohira@gmail.com>
2018-09-06 02:19:40 +1000

    Selecting groups working

--- chat-app/src/app/home/home.component.ts
+++ chat-app/src/app/home/home.component.ts
@@ -8,7 +8,9 @@ import {Router} from "@angular/router";
 })
 export class HomeComponent implements OnInit {
   public user;
+  public selectedGroup;
   public groups = [];
+  public channels = [];
 
   constructor(private router: Router) { }
 
@@ -29,5 +31,20 @@ export class HomeComponent implements OnInit {
     console.log('logout()');
     sessionStorage.clear();
     this.router.navigate(['/login']);
+  }
+
+  openGroup(name){
+    console.log(name);
+    // sessionStorage.setItem('currentGroup', name);
+    for(let i = 0; i < this.groups.length; i++){
+      if(this.groups[i].name == name){
+        this.selectedGroup = this.groups[i];
+      }
+    }
+  }
+
+  getChannels(groupName){
+    let channels = [];
+    return channels;
   }
 }
