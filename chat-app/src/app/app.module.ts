46bde6e32e94d50570330d5a55f3d5f17680719b
Ryoma Ohira <r.j.ohira@gmail.com>
2018-09-06 01:36:40 +1000

    Added channels component to home

--- chat-app/src/app/app.module.ts
+++ chat-app/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { HttpClientModule } from '@angular/common/http';
-import { ChannelsComponent } from './channels/channels.component'; 
+import { ChannelsComponent } from './channels/channels.component';
 
 @NgModule({
   declarations: [
