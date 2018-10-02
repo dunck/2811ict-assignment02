d8d09a5257ef3086554ce23c96d585cacd433e26
Duncan Kirkland <duncanck0@gmail.com>
2018-10-02 12:23:19 +1000

    Implemented Mongo-backend groups admin.

--- server/groups.js
+++ server/groups.js
@@ -3,92 +3,77 @@
 // groups and channels according to the user's 
 // responsibilities
 // ============================================
-module.exports = function(){
+
+
+module.exports = () => {
     this.username;
     this.data;
-    
+    this.mongo = require("mongodb").MongoClient;
+    this.url = "mongodb://localhost:27017/";
+    this.channels = require('./channels.js')();
+    this.login = require('./login.js')();
+
     // Find and delete a group by matching groupName to available data in this.data
-    this.deleteGroup = function(groupName){
-        let found = false;
-        //console.log(this.data);
-        for(let i = 0; i < this.data.length; i++){
-            if(this.data[i].name == groupName){
-                found = true;
-                this.data.splice(i, 1);
-                return this.data
-            }
+    this.deleteGroup = async (groupName) => {
+        let db = await this.mongo.connect(this.url, { useNewUrlParser: true });
+        let dbo = await db.db("chat-app");
+        let res = await dbo.collection("groups").deleteOne({ "name": groupName });
+        await db.close();
+        return res;
+    }
+
+    // Find and delete a group by matching groupName to available data in this.data
+    this.createGroup = async (groupName) => {
+        let db = await mongo.connect(this.url, { useNewUrlParser: true });
+        let dbo = await db.db("chat-app");
+
+        let group = {
+            'name': groupName,
+            'admins': ['super'],
+            'members': []
         }
-        return found;
+
+        // Don't create the group if it already exists.
+        let exists = await dbo.collection("groups").find({ "name": groupName }).toArray();
+        let res = false;
+        if (exists.length == 0) {
+            res = await dbo.collection("groups").insertOne(group);
+            console.log(await dbo.collection("groups").find().toArray());
+        }
+        else {
+            console.log(`Group already exists.`);
+        }
+        await db.close();
+        return res;
     }
 
     // Return all groups where the username exists (or according to role)
-    this.getGroups = function(username, role = 0){
-        let groups = [];
-        //console.log(role);
+    this.getGroups = async username => {
+        let db = await mongo.connect(this.url, { useNewUrlParser: true });
+        let dbo = await db.db("chat-app");
 
-        if(role == 2){
-            // Just return every group and every channel
-            for(let i = 0; i < data.groups.length; i++){
-                let group = data.groups[i];
-                group.channels = this.getChannels(username, group, role);
-                group.role = 2;
-                groups.push(group);
-            }
-        } else {   
-            // Check for group admin
-            for(let i = 0; i < data.groups.length; i++){
-                let admins = data.groups[i].admins;
-                for(let j = 0; j < admins.length; j++){
-                    if(username == admins[j]){
-                        data.groups[i].role = 1;
-                        groups.push(data.groups[i]);
-                    }
-                }
-            }
+        // 1 If the role is super-admin, return all the groups.
+        // 2 If the role is group-admin, return all the groups.
+        // 3 If the role is user, return all the groups where the user is a member.
+        let role = await this.login.getUserRole(username);
+        let searchbase = {};
+        if (role == 0)
+            searchbase = { "members": username };
 
-            // Check for membership
-            for(let i = 0; i < data.groups.length; i++){
-                let members = data.groups[i].members;
-                for(let j = 0; j < members.length; j++){
-                    if(username == members[j]){
-                        data.groups[i].role = 0;
-                        groups.push(data.groups[i]);
-                    }
-                }
-            }
+        let groups = await dbo.collection("groups").find(searchbase).toArray();
+        await db.close();
 
-            // Grab the channels for each group
-            for(let i = 0; i < groups.length; i++){
-                let channels = getChannels(username, groups[i], groups[i].permissions);
-                groups[i].channels = channels;
-            }
-        }
+        // Grab the channels for each group
+        // console.log('Channels:');
+        // for (let i = 0; i < groups.length; i++) {
+        //     let channels = await this.channels.getChannels(username, groups[i], role);
+        //     // console.log(channels);
+        //     groups[i].channels = channels;
+        // }
+        // console.log('End channels.');
+
         return groups;
     }
 
-    // Get all the channels a user has access for a given group and role
-    this.getChannels = function(username, group, role){
-        channels = [];
-        // Go through all the channels
-        for(let i = 0; i < data.channels.length; i++){
-            // Check to see if the channel matches the current group
-            if(data.channels[i].group == group.name){
-                if(role >= 2 || group.role >= 1){
-                    channels.push(data.channels[i]);
-                } else {
-                    // Channel belongs to group, check for access
-                    let channel = data.channels[i];
-                    for(let j = 0; j < channel.members.length; j++){
-                        if(username == channel.members[j]){
-                            channels.push(channel);
-                        }
-                    }
-                }
-            }
-        }
-
-
-        return channels;
-    }
     return this;
 }
