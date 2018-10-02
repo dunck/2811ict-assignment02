9d7b403d42d0c2fb448cd8a8f54e7711cfbbd942
Duncan Kirkland <duncanck0@gmail.com>
2018-10-02 16:23:32 +1000

    Commented all UserService functions.

--- chat-app/src/app/user.service.ts
+++ chat-app/src/app/user.service.ts
@@ -1,7 +1,6 @@
-// ============================================
-// This service is responsible for CRUD actions 
-// to the user APIs
-// ============================================
+// user.service.ts
+// UserService
+// Exposes functions for managing users via API calls.
 
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -19,16 +18,22 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
+  // Process user login by piping the username and password to the appropriate API route.
+  // Returns the result of that API call (truthy).
   login(data) {
     let body = JSON.stringify(data);
     return this.http.post(this.api + 'login', body, httpOptions);
   }
 
+  // Creates a user by piping the username, password & role to the appropriate API route.
+  // Returns the result of that API call (truthy).
   create(data) {
     let body = JSON.stringify(data);
     return this.http.post(this.api + 'user/create', body, httpOptions);
   }
 
+  // Deletes a user by piping the username to the appropriate API route.
+  // Returns the result of that API call (truthy).
   delete(username) {
     return this.http.delete(this.api + 'user/delete/' + username);
   }
