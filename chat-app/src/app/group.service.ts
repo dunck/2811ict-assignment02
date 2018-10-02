211b0838e4a80a4c1b2794722a1e044682c4f3e1
Duncan Kirkland <duncanck0@gmail.com>
2018-10-02 16:24:10 +1000

    Commented all GroupService functions.

--- chat-app/src/app/group.service.ts
+++ chat-app/src/app/group.service.ts
@@ -1,7 +1,6 @@
-// ============================================
-// This service is responsible for CRUD actions 
-// to the group APIs
-// ============================================
+// group.service.ts
+// GroupService
+// Exposes functions for managing groups via API calls.
 
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -19,40 +18,42 @@ export class GroupService {
 
   constructor(private http: HttpClient) { }
 
+  // Creates a group by piping `groupName` to the appropriate API route.
+  // Returns the result of the API call.
   createGroup(groupName) {
-    // let body = JSON.stringify(data);
     return this.http.post(this.api + 'group/create/' + groupName, httpOptions);
   }
 
+  // Creates a channel by piping `groupName` and `channelName` to the appropriate API route.
+  // Returns the result of the API call.
   createChannel(groupName, channelName) {
-    let body = { "groupName" : groupName, "channelName" : channelName };
+    let body = { "groupName": groupName, "channelName": channelName };
     return this.http.post(this.api + 'channel/create', body, httpOptions);
   }
 
-  deleteGroup(groupName, username) {
+  // Deletes a group by piping `groupName` to the appropriate API route.
+  // Returns the result of the API call.
+  deleteGroup(groupName) {
     return this.http.delete(this.api + 'group/delete/' + groupName);
   }
-
+  
+  // Deletes a channel by piping `channelName` to the appropriate API route.
+  // Returns the result of the API call.
   deleteChannel(channelName) {
     return this.http.delete(this.api + 'channel/delete/' + channelName);
   }
 
+  // Gets a list of groups + info by piping the logged-in user + role to the appropriate API route.
+  // Returns the result of the API call.
   getGroups(data) {
     let body = JSON.stringify(data);
-    console.log("getGroups(data)");
-    console.log(body);
-    let ret = this.http.post(this.api + 'groups', body, httpOptions);
-    // setTimeout(null, 3000);
-    // console.log(ret);
-    return ret;
+    return this.http.post(this.api + 'groups', body, httpOptions);
   }
 
+  // Gets all the channels relevant to the given username, group & role. 
+  // Returns the result of the API call.
   getChannels(username, group, role) {
     let body = JSON.stringify({ "group": group.name, "username": username, "role": role });
-    console.log(`Group:`);
-    console.log(body);
-    let ret = this.http.post(this.api + 'channels', body, httpOptions);
-    return ret;
+    return this.http.post(this.api + 'channels', body, httpOptions);
   }
-
 }
