251209201ea0b230bee4fee173cd33d1733e8800
Duncan Kirkland <duncanck0@gmail.com>
2018-10-02 12:23:36 +1000

    Stylistic fixes on server.js.

--- server/server.js
+++ server/server.js
@@ -21,8 +21,8 @@ var url = "mongodb://localhost:27017/";
 // our Node server. 
 const cors = require('cors')
 var corsOptions = {
-  origin: 'http://localhost:4200',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
 }
 app.use(cors(corsOptions))
 
