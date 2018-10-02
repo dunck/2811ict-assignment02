1813fe2dabf6d068a724a0c9c635f17e4bbc8261
Duncan Kirkland <duncanck0@gmail.com>
2018-10-02 12:37:18 +1000

    Implemented chat service, HTML&TS from socket.

---
+++ chat-app/src/app/chat.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { WebsocketService } from './websocket.service';
+import { Observable, Subject } from 'rxjs-compat/Rx';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+  
+  messages: Subject<any>;
+  
+  // Our constructor calls our wsService connect method
+  constructor(private wsService: WebsocketService) {
+    this.messages = <Subject<any>>wsService
+      .connect()
+      .map((response: any): any => {
+        return response;
+      })
+   }
+  
+  // Our simplified interface for sending
+  // messages back to our socket.io server
+  sendMsg(msg) {
+    this.messages.next(msg);
+  }
+  
+  disconnect() {
+    this.messages.disconnect();
+  }
+
+}
--- chat-app/src/app/chat/chat.component.ts
+++ chat-app/src/app/chat/chat.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { parse } from 'querystring';
+import { ChatService } from '../chat.service';
 
 @Component({
   selector: 'app-chat',
+  providers: [ChatService],
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
   @Input() channel;
-  constructor() { }
+  public message: String;
+  public chatHistory: Object[];
 
-  ngOnInit() {
+  constructor(private chat: ChatService) {
+    this.message = "";
+    this.chatHistory = [];
   }
 
+  ngOnInit() {
+    this.chat.messages.subscribe(msg => {
+      console.log(this.chatHistory);
+      if(msg.type == 'new-message') {
+        this.chatHistory.push(msg.data);
+      }
+      // console.log(msg);
+    })
+  }
+  
+  sendMessage() {
+    console.log("Sending message.");
+    let username = JSON.parse(sessionStorage.getItem('user')).username;
+    let channel = sessionStorage.getItem('selectedChannel');
+    let group = sessionStorage.getItem('selectedGroup');
+    this.chat.sendMsg({
+      "message": this.message,
+      "username": username,
+      "group": group,
+      "channel": channel
+    });
+  }
 }
