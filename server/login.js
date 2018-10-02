4d5c8f895c45ab6b6360b25fbdb6dc29c4fb1dc1
Duncan Kirkland <duncanck0@gmail.com>
2018-10-02 13:12:01 +1000

    Updated login to check password.

--- server/login.js
+++ server/login.js
@@ -9,10 +9,13 @@ module.exports = function () {
     this.mongo = require("mongodb").MongoClient;
     this.url = "mongodb://localhost:27017/";
 
-    this.findUser = async username => {
+    this.findUser = async (username, password) => {
         let db = await mongo.connect(this.url, { useNewUrlParser: true });
         let dbo = await db.db("chat-app");
-        let match = await dbo.collection("users").find({ "username": username }).toArray();
+        let match = await dbo.collection("users").find({ 
+            "username": username,
+            "password": password 
+        }).toArray();
         await db.close();
 
         if (match.length > 1)
