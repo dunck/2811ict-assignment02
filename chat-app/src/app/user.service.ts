88ac7a96b442981f680dec765233f80ea253ab7a
Duncan Kirkland <duncanck0@gmail.com>
2018-10-02 12:34:50 +1000

    Stylistic fixes to user service.

--- chat-app/src/app/user.service.ts
+++ chat-app/src/app/user.service.ts
@@ -5,7 +5,7 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {Observable, of} from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -15,21 +15,21 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class UserService {
-  private api:string = 'http://localhost:3000/api/';
+  private api: string = 'http://localhost:3000/api/';
 
-  constructor(private http:HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  login(data){
+  login(data) {
     let body = JSON.stringify(data);
     return this.http.post(this.api + 'login', body, httpOptions);
   }
 
-  create(data){
+  create(data) {
     let body = JSON.stringify(data);
     return this.http.post(this.api + 'user/create', body, httpOptions);
   }
 
-  delete(username){
-    return this.http.delete(this.api + 'user/delete/'+username);
+  delete(username) {
+    return this.http.delete(this.api + 'user/delete/' + username);
   }
 }
