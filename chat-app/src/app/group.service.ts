e5c35cdb1201b1d4b8249348533d5a53d5f91e4f
Duncan Kirkland <duncanck0@gmail.com>
2018-10-02 12:38:12 +1000

    Improved on group service to account for new functions.

--- chat-app/src/app/group.service.ts
+++ chat-app/src/app/group.service.ts
@@ -5,7 +5,7 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {Observable, of} from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -15,21 +15,44 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class GroupService {
-  private api:string = 'http://localhost:3000/api/';
+  private api: string = 'http://localhost:3000/api/';
 
-  constructor(private http:HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  createGroup(data){
-    let body = JSON.stringify(data);
-    return this.http.post(this.api + 'group/create', body, httpOptions);
+  createGroup(groupName) {
+    // let body = JSON.stringify(data);
+    return this.http.post(this.api + 'group/create/' + groupName, httpOptions);
   }
 
-  deleteGroup(groupName, username){
+  createChannel(groupName, channelName) {
+    let body = { "groupName" : groupName, "channelName" : channelName };
+    return this.http.post(this.api + 'channel/create', body, httpOptions);
+  }
+
+  deleteGroup(groupName, username) {
     return this.http.delete(this.api + 'group/delete/' + groupName);
   }
-  getGroups(data){
+
+  deleteChannel(channelName) {
+    return this.http.delete(this.api + 'channel/delete/' + channelName);
+  }
+
+  getGroups(data) {
     let body = JSON.stringify(data);
-    return this.http.post(this.api + 'groups', body, httpOptions);
+    console.log("getGroups(data)");
+    console.log(body);
+    let ret = this.http.post(this.api + 'groups', body, httpOptions);
+    // setTimeout(null, 3000);
+    // console.log(ret);
+    return ret;
+  }
+
+  getChannels(username, group, role) {
+    let body = JSON.stringify({ "group": group.name, "username": username, "role": role });
+    console.log(`Group:`);
+    console.log(body);
+    let ret = this.http.post(this.api + 'channels', body, httpOptions);
+    return ret;
   }
 
 }
